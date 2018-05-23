20fd9aa7d218d175cd83d0c351b21200fe6fe671
rossgales <ross.gales@gmail.com>
2018-05-23 19:17:11 +0100

    fixed compatibility with pip>10

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@
 #from distutils.core import setup
 import re, uuid
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
 
 VERSIONFILE = "tweepy/__init__.py"
 ver_file = open(VERSIONFILE, "rt").read()
